ee656842a2c273224796cf2c3fa2e1f185431ce8
levanawizman <Levana.WIZMAN@ortmontreuil.fr>
2024-10-28 15:59:24 +0100

    Evaluation1-levanawizman

--- src/main/java/com/sio/Main.java
+++ src/main/java/com/sio/Main.java
@@ -9,6 +9,7 @@ public class Main {
     private static final String RESET = "\u001B[0m";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(GREEN);
         System.out.println("#############################################");
         System.out.println("            JAVA EVAL 1 SIO 1 ALT            ");
@@ -27,52 +28,103 @@ public class Main {
             int choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch (choice){
+            switch (choice) {
                 case 1:
                     System.out.println("---------------------------------------------");
                     System.out.println("Exercice 1");
                     System.out.println("---------------------------------------------");
-                    //Exercice 1 : Entrer votre code ici
+                    System.out.println("Entrer un nombre :");
+                    double nombre = scanner.nextDouble();
+                    System.out.println("Entrer la puissance :");
+                    int puissance = scanner.nextInt();
+                    if (puissance < 0) {
+                        System.out.println("La puissance doit être supérieure ou égale à 0. Entrer la puissance :");
+                        puissance = scanner.nextInt();
+                    }
 
-
-
+                    double resultat = Math.pow(nombre, puissance);
+                    System.out.println("Le résultat de " + nombre + " puissance " + puissance + " est " + resultat);
                     System.out.println("---------------------------------------------");
                     break;
                 case 2:
                     System.out.println("---------------------------------------------");
                     System.out.println("Exercice 2");
                     System.out.println("---------------------------------------------");
-                    //Exercice 2 : Entrer votre code ici
+                    int kilometres = 0;
+                    do {
+                        System.out.print("Entrez le nombre de kilomètres parcourus :");
+                        kilometres = scanner.nextInt();
 
+                        if (kilometres <= 0) {;
+                        }
+                    } while (kilometres <= 0);
 
+                    double frais = 0.0;
 
+                    if (kilometres < 100) {
+                        frais = kilometres * 0.20;
+                    } else if (kilometres <= 500) {
+                        frais = kilometres * 0.50;
+                    } else {
+                        frais = kilometres * 0.80;
+                    }
 
-                    System.out.println("---------------------------------------------");
-                    break;
+                    System.out.println("Le montant à payer par le client est : " + frais + " €");
                 case 3:
+
                     System.out.println("---------------------------------------------");
                     System.out.println("Exercice 3");
                     System.out.println("---------------------------------------------");
-                    //Exercice 3 : Entrer votre code ici
+                    int largeur = 0;
+                    int hauteur = 0;
+                    do {
+                        System.out.print("Entrez la largeur du rectangle : ");
+                        largeur = scanner.nextInt();
+                        if (largeur <= 0) {
+                            System.out.println("La largeur doit être supérieure à 0.Entrez la largeur du rectangle : ");
+                        }
+                    } while (largeur <= 0);
+                    do {
+                        System.out.print("Entrez la hauteur du rectangle: ");
+                        hauteur = scanner.nextInt();
+                        if (hauteur <= 0) {
+                            System.out.println("La hauteur doit être supérieure à 0.Entrez la largeur du rectangle.");
+                        }
+                    } while (hauteur <= 0);
 
-
-
-
+                    for (int i = 0; i < hauteur; i++) {
+                        for (int j = 0; j < largeur; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
                     System.out.println("---------------------------------------------");
-
                     break;
-                case 4:
+                case 4 :
+
                     System.out.println("---------------------------------------------");
                     System.out.println("Exercice 4");
                     System.out.println("---------------------------------------------");
-                    //Exercice 4 : Entrer votre code ici
+                    int unnombre;
+                    do {
+                        System.out.print("Entrez un nombre : ");
+                        unnombre = scanner.nextInt();
 
+                        if (unnombre <= 0) {
+                            System.out.println("Le nombre doit être supérieur à 0. Entrer un nombre :");
+                        }
+                    } while (unnombre <= 0);
 
+                    long factorielle = 1;
+                    for (int i = 1; i <= unnombre; i++) {
+                        factorielle *= i;
+                    }
 
-
+                    System.out.println("La factorielle de " + unnombre + " est : " + factorielle);
                     System.out.println("---------------------------------------------");
                     break;
                 case 0:
+
                     System.out.println(RESET);
                     System.exit(0);
                     break;
